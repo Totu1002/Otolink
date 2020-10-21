a3254df4085a44443919697a47128b03f372f2e5
Totu1002_Mac <totu.enjoy.music@gmail.com>
2020-10-22 03:17:54 +0900

    [Add]ユーザー/記事検索機能実装

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -4,7 +4,6 @@ class Users::HomesController < ApplicationController
     #メンバー募集記事(10件)表示
     #ページネーションテスト用表示件数に変更中
     @recruit_members = Recruit.where(is_status: true,article_type: "メンバー募集").page(params[:page]).per(5)
-    #binding.pry
 
     #バンド募集記事(10件)表示
     @recruit_bands = Recruit.where(is_status: true,article_type: "バンド募集").page(params[:page]).per(5)
